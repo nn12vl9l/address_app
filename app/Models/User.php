df49df5b63be288165c7f3a8bee1c536c0daa893
nn12vl9l <nn12vl9lsoft@gmail.com>
2021-10-20 11:32:00 +0900

    gitHub認証完了

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Member::class);
     }
+
+    public function identityProvider()
+    {
+        return $this->hasOne(IdentityProvider::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MemberController;
+use App\Http\Controllers\Auth\OAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +30,14 @@ Route::resource('members', MemberController::class)
     ->only(['show', 'index']);
 
 require __DIR__ . '/auth.php';
+
+Route::prefix('auth')->middleware('guest')->group(function () {
+
+    Route::get('{provider}', [OAuthController::class, 'redirectToProvider'])
+        ->where('provider', 'github|google')
+        ->name('redirectToProvider');
+
+    Route::get('{prpvider}/callback', [OAuthController::class, 'oauthCallback'])
+        ->where('provider', 'github|google')
+        ->name('oauthCallback');
+});
